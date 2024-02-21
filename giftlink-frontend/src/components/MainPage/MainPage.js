363ded64d1172682a6f4a06f47ab49da78cab77c
lavskillup <73978832+lavskillup@users.noreply.github.com>
2024-02-22 10:54:29 +1100

    Update MainPage.js

--- giftlink-frontend/src/components/MainPage/MainPage.js
+++ giftlink-frontend/src/components/MainPage/MainPage.js
@@ -8,24 +8,19 @@ function MainPage() {
 
     useEffect(() => {
         // Task 1: Write async fetch operation
-        {{
-            //insert code here
-        }}
+        // Write your code below this line
     }, []);
 
     // Task 2: Navigate to details page
     const goToDetailsPage = (productId) => {
-        {{
-            //insert code here
-        }}
-    };
+        // Write your code below this line
+
+      };
 
     // Task 3: Format timestamp
     const formatDate = (timestamp) => {
-        {{
-            //insert code here
-        }}
-    };
+        // Write your code below this line
+      };
 
     const getConditionClass = (condition) => {
         return condition === "New" ? "list-group-item-success" : "list-group-item-warning";
@@ -37,27 +32,23 @@ function MainPage() {
                 {gifts.map((gift) => (
                     <div key={gift.id} className="col-md-4 mb-4">
                         <div className="card product-card">
-                            
-                            // Task 4: Display gift image or placeholder
-                            {{
-                                //insert code here
-                            }}
-                            
+
+                            {/* // Task 4: Display gift image or placeholder */}
+                            {/* // Write your code below this line */}
+
                             <div className="card-body">
+
+                                {/* // Task 5: Display gift image or placeholder */}
+                                {/* // Write your code below this line */}
+
+                                <p className={`card-text ${getConditionClass(gift.condition)}`}>
+                                {gift.condition}
+                                </p>
+
+                                {/* // Task 6: Display gift image or placeholder */}
+                                {/* // Write your code below this line */}
                                 
-                                // Task 5: Display gift name
-                                <h5 className="card-title">
-                                    {{
-                                        insert code here
-                                    }}
-                                </h5>
-                                
-                                // Task 6: Display formatted date
-                                <p className="card-text">
-                                {{
-                                    //insert code here
-                                }}</p>
-                                
+
                                 <button onClick={() => goToDetailsPage(gift.id)} className="btn btn-primary">
                                     View Details
                                 </button>
