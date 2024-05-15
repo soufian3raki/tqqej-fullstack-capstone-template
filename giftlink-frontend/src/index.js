7ac2460ced229c82fd5cde6450054c8412dceeb6
lavskillup <73978832+lavskillup@users.noreply.github.com>
2024-05-15 18:38:24 +0530

    Update index.js

--- giftlink-frontend/src/index.js
+++ giftlink-frontend/src/index.js
@@ -3,7 +3,6 @@ import ReactDOM from 'react-dom/client';
 import { BrowserRouter as Router } from 'react-router-dom';
 import './index.css';
 import App from './App';
-import reportWebVitals from './reportWebVitals';
 import {AuthProvider} from './context/AuthContext';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
@@ -17,4 +16,3 @@ root.render(
   </React.StrictMode>
 );
 
-reportWebVitals();
